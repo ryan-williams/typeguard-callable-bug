a12d502dd47da496092ee4a04d9acf234059e990
Ryan Williams <ryan@runsascoded.com>
2024-03-20 20:18:02 -0400

    fold callables.py into test_callables.py

--- src/test_callables.py
+++ src/test_callables.py
@@ -1,4 +1,22 @@
-from .callables import fn1, _fn1, _fn0, fn0
+from typing import Callable, Optional
+
+
+# ✅ type-checks fine
+def _fn0(fn: Callable[[int], bool], s: int) -> bool:
+    return fn(s)
+
+
+def fn0(s: int) -> bool:
+    return True
+
+
+# ❌ TypeCheckError: argument "fn" (function) has too many mandatory positional arguments in its declaration; expected 0 but 1 mandatory positional argument(s) declared
+def _fn1(fn: Callable[[Optional[int]], bool], s: Optional[int]) -> bool:
+    return fn(s)
+
+
+def fn1(s: Optional[int]) -> bool:
+    return True
 
 
 def test_fn0():
